df0ba9ca96a01c21a132e2031aea52484c4591a1
kevocs410 <kevocs410@yahoo.com>
2022-07-29 11:56:34 -0400

    changed file name to original names

--- app.js
+++ app.js
@@ -1,37 +1,68 @@
 $(() => {
 
 
+$(".socialIcon, .whatIDone, .myGoals, .whatIDoTitle, .followMeTitle").hide()
 
-$(".website").mouseenter(function(){
 
-    $(this).fadeOut(200);
-    $(this).fadeIn(200);
+
+$(".aboutButton").on("click",function(){
+    $(".whatIDone").hide()
+    $(".myGoals").hide()
+       $(".whatIDoTitle").hide()
+    $(".socialIcon").hide()
+    $(".followMeTitle").hide()
+$(".aboutMe").fadeIn(2000)
+$(".title").fadeIn(2000)
 })
 
 
+$(".leftButton").on("click",function(){
+    $(".whatIDone").hide()
+    $(".myGoals").hide()
+    $(".aboutMe").hide()
+    $(".title").hide()
+    $(".whatIDoTitle").hide()
+$(".socialIcon").fadeIn(2000)
+$(".followMeTitle").fadeIn(2000)
+})
 
-$(".aboutMe").mouseenter(function(){
-
-    $(this).fadeOut(200);
-    $(this).fadeIn(200);
+$(".middleButton").on("click",function(){
+    $(".socialIcon").hide()
+    $(".myGoals").hide()
+    $(".title").hide()
+    $(".aboutMe").hide()
+    $(".followMeTitle").hide()
+$(".whatIDone").fadeIn(2000)
+$(".whatIDoTitle").fadeIn(2000)
+})
+      
+    
+$(".rightButton").on("click",function(){
+    $(".socialIcon").hide()
+    $(".whatIDone").hide()
+    $(".followMeTitle").hide()
+    $(".whatIDoTitle").hide()
+    $(".aboutMe").hide()
+$(".myGoals").fadeIn(2000)
+$(".title").fadeIn(2000)
 })
 
 
-$(".entertainment").mouseenter(function(){
+$(".grapixDesign").on("mouseover",function(){
+    $(".grapixDesign").attr("src", "/Users/kevoproductions/Desktop/Port project/PortProject/r.jpeg")})
 
-    $(this).fadeOut(200);
-    $(this).fadeIn(200);
+    $(".grapixDesign").on("mouseleave",function(){
+        $(".grapixDesign").attr("src", "/Users/kevoproductions/Desktop/Port project/PortProject/desin.jpeg")})
+
+
+      
+$(".aboutMeHide").hide()
+
+
+        $(".aboutMeP").on("mouseover",function(){
+            $(".aboutMeHide").fadeIn(2000);
+        })
+       
+
 })
 
-  
-
-
-
-
-
-
-
-
-
-
-});
